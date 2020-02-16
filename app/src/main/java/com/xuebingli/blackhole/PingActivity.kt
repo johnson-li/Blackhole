33894362ff3f888889ba52803ce7f685b0f7ce9a
Johnson Li <johnsonli1993@gmail.com>
2020-02-17 01:49:15 +0200

    basic ping util

--- app/src/main/java/com/xuebingli/blackhole/PingActivity.kt
+++ app/src/main/java/com/xuebingli/blackhole/PingActivity.kt
@@ -1,6 +1,7 @@
 package com.xuebingli.blackhole
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -11,6 +12,9 @@ import com.google.android.material.tabs.TabLayoutMediator
 import com.xuebingli.blackhole.results.PingParsedResultFragment
 import com.xuebingli.blackhole.results.RawResultFragment
 import com.xuebingli.blackhole.results.ResultFragmentPair
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.nio.ByteBuffer
 
 val fragments = arrayOf(
     ResultFragmentPair("Raw Result") { RawResultFragment() },
@@ -35,7 +39,20 @@ class PingActivity : BaseActivity(true) {
     }
 
     fun pingAction(view: View) {
-
+        val process = Runtime.getRuntime().exec("ping 1.1.1.1")
+        val output = DataOutputStream(process.outputStream)
+        val input = DataInputStream(process.inputStream)
+        val error = DataInputStream(process.errorStream)
+        Thread {
+            val buffer = ByteBuffer.allocate(Short.MAX_VALUE.toInt())
+            while (process.isAlive) {
+                val length = input.read(buffer.array())
+                if (length > 0) {
+                    val data = String(buffer.array(), 0, length)
+                    Log.d("johnson", "data: $data")
+                }
+            }
+        }.start()
     }
 }
 
